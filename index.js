ce57044467051e01e92f8d698e107dac4f4e3ff8
VandelayUtd <drewhadley.guitar@gmail.com>
2021-10-20 16:44:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+
+function writeCards(namesArray, eventString){
+    let cards = []
+    for (let i = 0; i < namesArray.length; i++ ){
+        cards.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventString} gift!`);
+    }
+    return cards
+}
+
+function countDown(int){
+    while (int > -1) {
+        console.log(int--) 
+    }
+}
